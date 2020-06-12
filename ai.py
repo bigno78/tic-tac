85b6ed40642cc9f68489fd6993802203c2f75dff
Miroslav Demek <469364@mail.muni.cz>
2020-06-12 10:10:40 +0200

    Improve AI

--- ai.py
+++ ai.py
@@ -8,31 +8,41 @@ def count_score(board, i, j):
     cnt = 0
     for x, y in [ (0, 1), (1, 0), (1, 1), (1, -1) ]:
         val = board.count_dir(i, j, x, y)
+        if j == 3:
+            #print(val)
+            pass
         if val >= Game.win_count:
-            #print("bruh", board.at(i, j), i, j)
+            #print("bruh", board.at(i, j), board.h - 1 - i, j)
             return math.inf
         cnt += val
     return cnt
 
-
+o = True
 def eval_board(board, player: int):
     score = 0
-
+    global o
     for j in range(board.w):
         i = board.h - 1
+        #print(i)
         while i >= 0 and board.at(i, j) != None:
+            #print("x")
             if player == board.at(i, j):
                 score += count_score(board, i, j)
             else:
                 score -= count_score(board, i, j)
+            if o:
+                print(score)
             i -= 1
-
+    
+    o = False
     return score
 
-
+states = 0
 def next_move(g):
-    val = minimax(g.board, 5, g.curr, g.curr)[1]
-    #print("\n")
+    global states
+    states = 0
+    val = minimax(g.board, 7, g.curr, g.curr, -math.inf, math.inf)[1]
+    print("states visited:", states)
     return val
 
 
@@ -40,6 +50,49 @@ def other_player(player: int):
     return (player + 1) % 2
 
 
+def minimax(board, depth, my_idx, player_idx, alpha, beta):
+    global states
+    states += 1
+    if depth == 0:
+        return (eval_board(board, my_idx), None)
+
+    maximize_score = (player_idx == my_idx) 
+    mult = 1 if maximize_score else -1
+
+    best = -mult*math.inf
+    move = None
+
+    for col in range(board.w):
+        if board.column_full(col):
+            continue
+        
+        board.push(col, player_idx)
+        if board.has_at_least(board.top_idx(col), col, Game.win_count):
+                board.pop(col)
+                return (mult*math.inf, col)
+
+        score, _ = minimax(board, depth-1, my_idx, other_player(player_idx), alpha, beta)
+        board.pop(col)
+
+        if move is None or mult*score > mult*best:
+            move = col
+            best = score
+
+        #print("               "*depth, col, best, "turn:", player_idx)
+
+        if maximize_score:
+            alpha = max(alpha, score)
+        else:
+            beta = min(beta, score)
+
+        if beta <= alpha:
+            break
+
+    #if best == -math.inf:
+    #    print("u gonna lose!")
+
+    return (best, move)
+"""
 def minimax(board, depth, my_idx: int, player_idx: int):
     if depth == 0:
         return (eval_board(board, my_idx), None)
@@ -83,7 +136,7 @@ def minimax(board, depth, my_idx: int, player_idx: int):
                 best = score
            # print("min:", score, "best:", best)
         return (best, move)
-
+"""
 
 def  simle():
     move = None
